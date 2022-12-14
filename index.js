a22763e72591e8d0a82d18ba6c43d12f593cfbc9
Nadia Virjee <nadvirjee@gmail.com>
2022-12-14 10:50:01 -0500

    commit tree 2c7da4871ceac46b1626f5248a78cdd3db4d721e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(customerLocation){
+    return Math.abs(customerLocation-42)
+};
+
+function distanceFromHqInFeet(customerLocation){
+    return distanceFromHqInBlocks(customerLocation)*264
+};
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((destination*264) - (start*264))
+};
+
+function calculatesFarePrice(start, destination) {
+    if (Math.abs((destination*264) - (start*264))<=400) 
+        return 0
+};
